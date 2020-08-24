6dd1cc5b76ab05b92d93146b9c4e95e1ae3d9c47
Omotayo Ema <mikey.obrian@yahoo.com>
2020-08-24 15:51:40 +0100

    basic styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   
   <div>
+    <div class="navigation"><p>Star Wars Characters</p></div>
     <div v-for="(character, index) in characters" :key='index'>
         <character :data='character' :id='index + 1' />
     </div>
@@ -26,6 +27,17 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+
+.navigation {
+  align-items: center;
+  color: #212121;
+  display: flex;
+  font-family: 'comfortaa', 'Open sans';
+  font-size: 30px;
+  font-weight: 900;
+  justify-content: center;
+  height: 100px;
+}
 
 </style>
  
